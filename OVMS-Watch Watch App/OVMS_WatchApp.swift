da88406627d4ac58b65d24f409d16bd5405d5965
Peter Harry <Peter.harry56@gmail.com>
2022-08-06 09:18:40 +1000

    Initial Commit

---
+++ OVMS-Watch Watch App/OVMS_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  OVMS_WatchApp.swift
+//  OVMS-Watch Watch App
+//
+//  Created by Peter Harry on 6/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct OVMS_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
